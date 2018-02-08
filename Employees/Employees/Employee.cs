d46e34a70b54217e7130744802c61b4bfacbca44
Gleb999 <lihachev99gleb@mail.ru>
2018-02-08 18:50:43 +0700

    The second principle of inheritance

---
+++ Employees/Employees/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    partial class Employee
+    {
+        
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+            :this( name, 0, id, pay) { }
+
+        public Employee(string name, int age, int id, float pay, string ssn)
+        {
+            empName = name;
+            empID = id;
+            empAge = age;
+            currPay = pay;
+            empSSN = ssn;
+        }
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Age: {0}", empAge);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}
---
+++ Employees/Employees/Employee2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class Employee2
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        public string value;
+        private int empAge;
+        private string empSSN;
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get { return empSSN; }
+        }
+        public string Name()
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                {
+                    Console.WriteLine("Error! Name must be less than 16 Characters!");
+                }
+                else
+                {
+                    empName = value;
+                }
+            }
+        }
+
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public string GetName()
+        {
+            return empName;
+        }
+
+        public void SetName(string name)
+        {
+            if (name.Length > 15)
+            {
+                Console.WriteLine("Error! Name must be less than 16 characters!");
+                else{
+                    empName = name;
+                }
+            }
+        }
+    }
+}
